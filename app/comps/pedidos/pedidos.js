5e1ac571593d685034c0d4b9908c9f1bd5ce8acf
Kerby Tabares <krtabares@gmail.com>
2020-07-08 17:35:53 -0400

    input btn

--- app/comps/pedidos/pedidos.js
+++ app/comps/pedidos/pedidos.js
@@ -31,9 +31,9 @@ angular.module('app.pedidos', ['datatables', 'datatables.buttons', 'datatables.b
           
         }
 
-        $scope.selectCLient = function(x){
+        $scope.selectCLient = function(){
           console.log(x)
-          $scope.client = x 
+          // $scope.client = x 
             $scope.pedido.no_cia = $scope.client.cod_cia;
             $scope.pedido.grupo = $scope.client.grupo_client
             $scope.pedido.no_cliente = $scope.client.cod_cliente
