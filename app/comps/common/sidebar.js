d01edba5444a167125e0be4ebea5a050de8122c5
Kerby Tabares <krtabares@gmail.com>
2020-07-23 11:32:06 -0400

    upd.

---
+++ app/comps/common/sidebar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('app.sidebar', ['ngRoute', 'ngNotify', 'ngMap', 'angular-bind-html-compile', 'ngStorage'])
+.component("mySidebar", {
+    templateUrl: "comps/common/sidebar.htmml"
+  })
+
+  .controller('sidebarCtrl', ['$scope', '$rootScope', '$routeParams', '$interval', '$timeout', 'ngNotify', 'localstorage', 'request', 'NgMap','$localStorage',
+    function($scope, $rootScope, $routeParams, $interval, $timeout, ngNotify, localstorage, request, NgMap, $localStorage) {
+
+        console.log("sidebarCtrl")
+    }
+  ]);
