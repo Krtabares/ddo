38bd8b88af9e36e14089b1d15e82b7bee2677e42
Kerby Tabares <krtabares@gmail.com>
2020-07-23 16:33:36 -0400

    client header

--- app/comps/clientHeader/clientHeader.js
+++ app/comps/clientHeader/clientHeader.js
@@ -23,7 +23,7 @@ angular.module('app.headerClient', ['ngRoute', 'ngNotify', 'ngMap', 'angular-bin
             var client = localStorage.getItem('client')
             var client_info = localStorage.getItem('client_info')
             console.log(client)
-            console.log( Object.keys(client).length === 0 )
+            console.log( Object.keys(client).length  )
            if ( Object.keys(client).length === 0 ){
                 $scope.hasClient = false;
             }else{
