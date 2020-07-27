aa16e28091df14d1d4d48085125bed13504efaad
Kerby Tabares <krtabares@gmail.com>
2020-07-27 16:28:18 -0400

    fix

--- app/comps/usuarios/usuarios.js
+++ app/comps/usuarios/usuarios.js
@@ -98,29 +98,29 @@ angular.module('app.usuarios', ['datatables', 'datatables.buttons', 'datatables.
      }
 
       $scope.addUser = function(user){
-      if (!validaForm()) {
-        return ;
-      }
-      user.password = "ddo.2017";
-      if($scope.clientIndex!=-1 && user.role == 'cliente'){
-        user.COD_CIA = $scope.client.cod_cia
-        user.GRUPO_CLIENTE = $scope.client.grupo_cliente
-        user.COD_CLIENTE = $scope.client.cod_cliente
-      }
-      console.log(user);
-      request.post(ip+'/add/user', user,{'Authorization': 'Bearer ' + localstorage.get('token')})
-          .then(function successCallback(response) {
-            console.log(response)
-            if (response.data == "OK") {
-			             $scope.initDatatable();
-              ngNotify.set('¡Usuario registrado exitosamente!','success')
-            } else if (response.data.email_flag) {
-              //ngNotify.set('¡Ya el correo está registrado!','error')
-            }
-          }, function errorCallback(response) {
-            console.log(response)
-          });
-      }
+          if (!validaForm()) {
+            return ;
+          }
+          user.password = "ddo.2017";
+          if($scope.clientIndex!=-1 && user.role == 'cliente'){
+            user.COD_CIA = $scope.client.cod_cia
+            user.GRUPO_CLIENTE = $scope.client.grupo_cliente
+            user.COD_CLIENTE = $scope.client.cod_cliente
+          }
+          console.log(user);
+          request.post(ip+'/add/user', user,{'Authorization': 'Bearer ' + localstorage.get('token')})
+              .then(function successCallback(response) {
+                console.log(response)
+                if (response.data == "OK") {
+    			             $scope.initDatatable();
+                  ngNotify.set('¡Usuario registrado exitosamente!','success')
+                } else if (response.data.email_flag) {
+                  //ngNotify.set('¡Ya el correo está registrado!','error')
+                }
+              }, function errorCallback(response) {
+                console.log(response)
+              });
+        }
 
         $scope.listUser = function(){
           request.get(ip+'/get/user', {})
@@ -135,7 +135,7 @@ angular.module('app.usuarios', ['datatables', 'datatables.buttons', 'datatables.
 
       function validaForm() {
 
-        if (!$scope.hasClient) {
+        if (Object.keys($scope.client).length === 0) {
           ngNotify.set('Seleccione un cliente','warn')
           return false;
         }
