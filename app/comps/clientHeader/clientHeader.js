058f1e08616b802e943a00d95bea4bf11cf63754
Kerby Tabares <krtabares@gmail.com>
2020-08-07 14:31:08 -0400

    valida client header

--- app/comps/clientHeader/clientHeader.js
+++ app/comps/clientHeader/clientHeader.js
@@ -16,6 +16,9 @@ angular.module('app.headerClient', ['ngRoute', 'ngNotify', 'ngMap', 'angular-bin
         $scope.client = {};
         $scope.client_info = {}
         $scope.dtInstance = {};
+        var ip = "http://192.168.168.170:3500";
+        $scope.clienteValido = true
+        $scope.clientInvalidoMsg = null
         verificClient()
 
         function verificClient(){
@@ -30,8 +33,45 @@ angular.module('app.headerClient', ['ngRoute', 'ngNotify', 'ngMap', 'angular-bin
                 $scope.hasUserClient = true;
                 $scope.client_info = JSON.parse(client_info);
                 $scope.client = JSON.parse(client);
+                var body = {}
+                body.pCliente = $scope.client_info.cod_cliente
+                body.pNoCia = $scope.client_info.cod_cia
+                body.pNoGrupo =  $scope.client_info.grupo_cliente
+
+                validaClienteDDO(body)
+
 
             }
+        }
+
+        function validaClienteDDO(body) {
+          console.log("validaClienteDDO");
+          request.post(ip+'/valida/client', body,{'Authorization': 'Bearer ' + localstorage.get('token', '')})
+          .then(function successCallback(response) {
+            console.log(response.data.data)
+
+            // $scope.creditoClient = response.data.obj
+            // $scope.creditoClient.disp_bs_format = parseFloat(response.data.obj.disp_bs)
+            // $scope.creditoClient.disp_usd_format = parseFloat(response.data.obj.disp_usd)
+            if(!response.data.data[0]){
+              $scope.clienteValido = true
+              $scope.clientInvalidoMsg = null
+            }else{
+              $scope.clientInvalidoMsg = response.data.data[0]
+              ngNotify.set($scope.clientInvalidoMsg,'warn')
+              $scope.clienteValido = false
+              $scope.tabsIndex = 0
+              return;
+            }
+
+
+
+
+
+
+          }, function errorCallback(response) {
+            console.log(response)
+          });
         }
     }
   ]);
