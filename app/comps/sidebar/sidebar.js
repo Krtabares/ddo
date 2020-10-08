283b732a65b85b1493665dcfb52bc147c6f53281
Kerby Tabares <krtabares@gmail.com>
2020-10-08 17:40:19 -0400

    fixx

--- app/comps/sidebar/sidebar.js
+++ app/comps/sidebar/sidebar.js
@@ -19,45 +19,73 @@ angular.module('app.mySidebar', ['ngRoute', 'ngNotify', 'ngMap', 'angular-bind-h
           var user = localStorage.getItem('user')
            $scope.user = JSON.parse(user);
            $scope.permisos = $scope.user.permisos  ;
+
+
            // console.log($scope.user)
         }
 
+/** comienza el encript*/
 
-        // jQuery(function ($) {
-        //
-        //       $(".sidebar-dropdown > a").click(function() {
-        //     $(".sidebar-submenu").slideUp(200);
-        //     if (
-        //       $(this)
-        //         .parent()
-        //         .hasClass("active")
-        //     ) {
-        //       $(".sidebar-dropdown").removeClass("active");
-        //       $(this)
-        //         .parent()
-        //         .removeClass("active");
-        //     } else {
-        //       $(".sidebar-dropdown").removeClass("active");
-        //       $(this)
-        //         .next(".sidebar-submenu")
-        //         .slideDown(200);
-        //       $(this)
-        //         .parent()
-        //         .addClass("active");
-        //     }
-        //   });
-        //
-        //   $("#close-sidebar").click(function() {
-        //     $(".page-wrapper").removeClass("toggled");
-        //   });
-        //   $("#show-sidebar").click(function() {
-        //     $(".page-wrapper").addClass("toggled");
-        //   });
-        //
-        //
-        //
-        //
-        // });
+$(function(){
+  var keySize = 256;
+  var ivSize = 128;
+  var iterations = 100;
+
+  var message = "1";
+  var password = "Secret Password";
+
+
+  function encrypt (msg, pass) {
+    var salt = CryptoJS.lib.WordArray.random(128/8);
+
+    var key = CryptoJS.PBKDF2(pass, salt, {
+        keySize: keySize/32,
+        iterations: iterations
+      });
+
+    var iv = CryptoJS.lib.WordArray.random(128/8);
+
+    var encrypted = CryptoJS.AES.encrypt(msg, key, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+
+    });
+
+    // salt, iv will be hex 32 in length
+    // append them to the ciphertext for use  in decryption
+    var transitmessage = salt.toString()+ iv.toString() + encrypted.toString();
+    return transitmessage;
+  }
+
+  function decrypt (transitmessage, pass) {
+    var salt = CryptoJS.enc.Hex.parse(transitmessage.substr(0, 32));
+    var iv = CryptoJS.enc.Hex.parse(transitmessage.substr(32, 32))
+    var encrypted = transitmessage.substring(64);
+
+    var key = CryptoJS.PBKDF2(pass, salt, {
+        keySize: keySize/32,
+        iterations: iterations
+      });
+
+    var decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7,
+      mode: CryptoJS.mode.CBC
+
+    })
+    return decrypted;
+  }
+
+  $('#encrypted').text("Encrypted: "+encrypted);
+  $('#decrypted').text("Decrypted: "+ decrypted.toString(CryptoJS.enc.Utf8) );
+
+
+})
+
+
+/** termina el encript*/
+
         $scope.side = true
 
         $scope.showSidebar = function(){
