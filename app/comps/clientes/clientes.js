af8a7dd9771f06f6587e2572889ed336de3347bf
Kerby Tabares <krtabares@gmail.com>
2020-10-09 09:09:07 -0400

    fix

--- app/comps/clientes/clientes.js
+++ app/comps/clientes/clientes.js
@@ -92,31 +92,31 @@ angular.module('app.clientes', ['datatables', 'datatables.buttons', 'datatables.
       .withPaginationType('full_numbers')
       .withLanguage(DATATABLE_LANGUAGE_ES)
       .withDisplayLength(15)
-
-        $scope.dtColumns = [
-
-
-            // DTColumnBuilder.newColumn('nombre_cia').withTitle('Cliente'),
-            DTColumnBuilder.newColumn('cod_cliente').withTitle('Codigo'),
-            DTColumnBuilder.newColumn('nombre_cliente').withTitle('Nombre'),
-            DTColumnBuilder.newColumn('docu_identif_cliente').withTitle('Documento de identificación'),
-            DTColumnBuilder.newColumn('ci').withTitle('Zona'),
-            DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Fiscal'),
-            DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Entrega'),
-            /*DTColumnBuilder.newColumn('nombre_cia').withTitle('Nombre Cia'),*/
-            // DTColumnBuilder.newColumn('grupo_cliente').withTitle('Grupo del cliente'),
-            DTColumnBuilder.newColumn('nombre_encargado').withTitle('Nombre del encargado').withClass('none'),
-            // DTColumnBuilder.newColumn('nom_grupo_cliente').withTitle('Nombre de grupo del cliente').withClass('none'),
-            DTColumnBuilder.newColumn('telefono1').withTitle('telefono1').withClass('none'),
-            // DTColumnBuilder.newColumn('telefono2').withTitle('telefono2').withClass('none'),
-            // DTColumnBuilder.newColumn('telefono3').withTitle('telefono3').withClass('none'),
-            // DTColumnBuilder.newColumn('telefono4').withTitle('telefono4').withClass('none'),
-            DTColumnBuilder.newColumn('email1').withTitle('email1').withClass('none'),
-            // DTColumnBuilder.newColumn('email2').withTitle('email2').withClass('none'),
-            // DTColumnBuilder.newColumn('email3').withTitle('email3').withClass('none'),
-            // DTColumnBuilder.newColumn('email4').withTitle('email4').withClass('none'),
-            // DTColumnBuilder.newColumn('v_persona_cyc').withTitle('Credito y cobranzas').withClass('none')
-        ];
+        // 
+        // $scope.dtColumns = [
+        //
+        //
+        //     // DTColumnBuilder.newColumn('nombre_cia').withTitle('Cliente'),
+        //     DTColumnBuilder.newColumn('cod_cliente').withTitle('Codigo'),
+        //     DTColumnBuilder.newColumn('nombre_cliente').withTitle('Nombre'),
+        //     DTColumnBuilder.newColumn('docu_identif_cliente').withTitle('Documento de identificación'),
+        //     DTColumnBuilder.newColumn('ci').withTitle('Zona'),
+        //     DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Fiscal'),
+        //     DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Entrega'),
+        //     /*DTColumnBuilder.newColumn('nombre_cia').withTitle('Nombre Cia'),*/
+        //     // DTColumnBuilder.newColumn('grupo_cliente').withTitle('Grupo del cliente'),
+        //     DTColumnBuilder.newColumn('nombre_encargado').withTitle('Nombre del encargado').withClass('none'),
+        //     // DTColumnBuilder.newColumn('nom_grupo_cliente').withTitle('Nombre de grupo del cliente').withClass('none'),
+        //     DTColumnBuilder.newColumn('telefono1').withTitle('telefono1').withClass('none'),
+        //     // DTColumnBuilder.newColumn('telefono2').withTitle('telefono2').withClass('none'),
+        //     // DTColumnBuilder.newColumn('telefono3').withTitle('telefono3').withClass('none'),
+        //     // DTColumnBuilder.newColumn('telefono4').withTitle('telefono4').withClass('none'),
+        //     DTColumnBuilder.newColumn('email1').withTitle('email1').withClass('none'),
+        //     // DTColumnBuilder.newColumn('email2').withTitle('email2').withClass('none'),
+        //     // DTColumnBuilder.newColumn('email3').withTitle('email3').withClass('none'),
+        //     // DTColumnBuilder.newColumn('email4').withTitle('email4').withClass('none'),
+        //     // DTColumnBuilder.newColumn('v_persona_cyc').withTitle('Credito y cobranzas').withClass('none')
+        // ];
 
 
   }]);
