7b3f2bde1ff7008931df5f63c9f192e4313d96c6
Kerby Tabares <krtabares@gmail.com>
2020-10-09 08:58:09 -0400

    fix

--- app/comps/clientes/clientes.js
+++ app/comps/clientes/clientes.js
@@ -17,28 +17,81 @@ angular.module('app.clientes', ['datatables', 'datatables.buttons', 'datatables.
 
       $scope.deuda = {};
       $scope.listDeuda = [{}];
+      $scope.clientes = null
+      $scope.clientIndex = null
+      $scope.showProductTable = null
+      $scope.loading = false
+      $scope.nombre_cliente=null
+      $scope.loading = true
 
-	$scope.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-        var body = {}
-        var defer = $q.defer();
-         request.post(ip+'/procedure_clientes', {'page': 1, }, {'Authorization': 'Bearer ' + localstorage.get('token', '')})
-          .then(function successCallback(response) {
-            console.log(response.data)
-			defer.resolve(response.data.obj);
-         });
-        return defer.promise;
-		})
-		.withDOM('frtip')
-        .withPaginationType('full_numbers')
-		.withButtons([
+      $scope.getClientNew = function (filter = false) {
+        // console.log("getClientNew");
+        // $scope.listaPedidosV2 = []
+        $scope.loading = true
+        var body = {};
+        if(filter){
+          body.pNombre = $scope.nombre_cliente
+        }
+        // body.pNoCia = "01";
+        // body.pNoGrupo = ($scope.tipoBusquedaCliente != 0)? "02": "01";
 
-            'pdf',
-            'excel'
-        ])
-		.withOption('responsive', true)
-    .withLanguage(DATATABLE_LANGUAGE_ES)
+        request.post(ip+'/procedure_clientes', body,{})
+        .then(function successCallback(response) {
+          // console.log(response)
+          $scope.clientes = null
+          $scope.clientIndex = null
+          $scope.showProductTable = null
+          $scope.loading = false
 
-    ;
+          if(response.data.obj.length > 0){
+
+            $scope.clientes = response.data.obj
+            // $scope.clientes = null
+            // $scope.clientIndex = -1
+            // $scope.nombre_cliente = null
+
+          }else{
+            // notify({ message:'¡No se encontraron resultados!', position:'right', duration:10000, classes:'alert-warning'});
+
+          }
+
+
+
+        }, function errorCallback(response) {
+          // console.log(response)
+          $scope.loading = false
+        });
+      }
+
+	// $scope.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+  //       var body = {}
+  //       var defer = $q.defer();
+  //        request.post(ip+'/procedure_clientes', {'page': 1, }, {'Authorization': 'Bearer ' + localstorage.get('token', '')})
+  //         .then(function successCallback(response) {
+  //           console.log(response.data)
+	// 		defer.resolve(response.data.obj);
+  //        });
+  //       return defer.promise;
+	// 	})
+	// 	.withDOM('frtip')
+  //       .withPaginationType('full_numbers')
+	// 	.withButtons([
+  //
+  //           'pdf',
+  //           'excel'
+  //       ])
+	// 	.withOption('responsive', true)
+  //   .withLanguage(DATATABLE_LANGUAGE_ES)
+  //
+  //   ;
+
+  $scope.dtOptions = DTOptionsBuilder.newOptions()
+      .withPaginationType('full_numbers')
+      .withOption('responsive', true)
+      .withDOM('frtip')
+      .withPaginationType('full_numbers')
+      .withLanguage(DATATABLE_LANGUAGE_ES)
+      .withDisplayLength(15)
 
         $scope.dtColumns = [
 
@@ -47,7 +100,7 @@ angular.module('app.clientes', ['datatables', 'datatables.buttons', 'datatables.
             DTColumnBuilder.newColumn('cod_cliente').withTitle('Codigo'),
             DTColumnBuilder.newColumn('nombre_cliente').withTitle('Nombre'),
             DTColumnBuilder.newColumn('docu_identif_cliente').withTitle('Documento de identificación'),
-            DTColumnBuilder.newColumn('zona').withTitle('Zona'),
+            DTColumnBuilder.newColumn('ci').withTitle('Zona'),
             DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Fiscal'),
             DTColumnBuilder.newColumn('direccion_cliente').withTitle('Direccion Entrega'),
             /*DTColumnBuilder.newColumn('nombre_cia').withTitle('Nombre Cia'),*/
