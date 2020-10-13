845aa373c5ede69d63c8209656096d8954a7c499
Kerby Tabares <krtabares@gmail.com>
2020-10-13 09:57:30 -0400

    fix

--- app/comps/pedidos/pedidos.js
+++ app/comps/pedidos/pedidos.js
@@ -637,7 +637,8 @@ angular.module('app.pedidos', ['datatables', 'datatables.buttons', 'datatables.b
 
             if($scope.proveedor.cod_proveedor != null || $scope.proveedor.cod_proveedor != "null" ){
               body.pCodProveedor = $scope.proveedor.cod_proveedor
-            }else if($scope.proveedor.cod_proveedor === "null"){
+            }
+             if(body.pCodProveedor === "null"){
               body.pCodProveedor = null
             }
 
