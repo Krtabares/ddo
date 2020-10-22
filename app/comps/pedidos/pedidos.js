349a51af5897d85fc7400065b1089a4de2a76d5b
Kerby Tabares <krtabares@gmail.com>
2020-10-22 08:49:11 -0400

    fix

--- app/comps/pedidos/pedidos.js
+++ app/comps/pedidos/pedidos.js
@@ -239,7 +239,7 @@ angular.module('app.pedidos', ['datatables', 'datatables.buttons', 'datatables.b
         $scope.selectCLient = function(){
 
           // $scope.client = x
-          if($scope.clientes.length > 0){
+          if($scope.client != null && $scope.clientes.length > 0){
               var auxCli = $scope.clientes
               var auxCliIndex = $scope.clientIndex
 
