72cca68aeb2a0f20a0a5291b7a042ea42a75a02b
Kerby Tabares <krtabares@gmail.com>
2020-12-21 10:52:11 -0400

    fix

--- server/smtpddo.py
+++ server/smtpddo.py
@@ -135,7 +135,7 @@ async def _send_email2(data, mypass):
                             </tr>
                             <tr>
                                 <td style="text-align:center;">
-                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.rakeshmandal.com</strong></p>
+                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>http://www.del-oeste.com</strong></p>
                                 </td>
                             </tr>
                             <tr>
