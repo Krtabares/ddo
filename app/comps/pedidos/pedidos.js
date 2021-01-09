1429132ecaa987a8cb3e71bc0b7be493d345aed8
Kerby Tabares <krtabares@gmail.com>
2021-01-08 21:23:39 -0400

    fix

--- app/comps/pedidos/pedidos.js
+++ app/comps/pedidos/pedidos.js
@@ -1788,8 +1788,8 @@ angular.module('app.pedidos', ['datatables', 'datatables.buttons', 'datatables.b
         $scope.$on('timer-tick', function (event, args) {
           // $scope.timerConsole += $scope.timerType  + ' - event.name = '+ event.name + ', timeoutId = ' + args.timeoutId + ', millis = ' + args.millis +'\n';
           // console.log(args.millis)
-
-          var ms = args.millis % 1000;
+          s = args.millis
+          var ms = s % 1000;
           s = (s - ms) / 1000;
           console.log($scope.msToTime(args.millis));
           if(s == 1 ){
