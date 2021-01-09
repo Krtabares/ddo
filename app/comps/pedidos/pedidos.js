be7ce1da1069d15f15d96c487d56b383be8ef0d4
Kerby Tabares <krtabares@gmail.com>
2021-01-08 23:02:47 -0400

    fix

--- app/comps/pedidos/pedidos.js
+++ app/comps/pedidos/pedidos.js
@@ -1803,9 +1803,35 @@ angular.module('app.pedidos', ['datatables', 'datatables.buttons', 'datatables.b
           }
 
         });
+        $scope.zoomImg = function (imgId, src) {
+
+                  // Get the modal
+          var modal = document.getElementById("modalImg");
+
+          // Get the image and insert it inside the modal - use its "alt" text as a caption
+          var img = document.getElementById(imgId);
+          var modalImg = document.getElementById("img01");
+          var captionText = document.getElementById("caption");
+          // img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = img.src;
+          captionText.innerHTML = img.alt;
+          // }
+
+          // Get the <span> element that closes the modal
+          var span = document.getElementsByClassName("close")[0];
+
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function() {
+            modal.style.display = "none";
+          }
+          
+        }
+
+
+
+
     }
-
-
 ])
 // .config(function(IdleProvider, KeepaliveProvider) {
 //   KeepaliveProvider.interval(10);
